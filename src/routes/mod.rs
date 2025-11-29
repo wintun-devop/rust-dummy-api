e6ac66259063345ecf01c1bb86272d7ed2a7d712
wintun-devop <wintun.hlaing@outlook.com>
2025-11-29 15:29:06 +0630

    modular

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,10 @@
 use axum::Router;
 
 mod health;
+mod items;
 
 pub fn create_router() -> Router {
-    Router::new().merge(health::router())
+    Router::new()
+        .merge(health::router())
+        .nest("/items", items::router())
 }
