9a8d0cdee8319bcc1f02a6eeb94e21017aae5a55
wintun-devop <wintun.hlaing@outlook.com>
2025-12-02 22:09:36 +0630

    base_path_with_modular_route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,25 +1,20 @@
 use axum::Router;
 
+use crate::config::config;
+
 mod health;
 mod items;
 
 pub fn create_router() -> Router {
-   /*
-    //old structure
-    Router::new()
-        .merge(health::router())
-        .nest("/items", items::router()) 
-    */
-
+    let api_base_path = config().api_base_path;
     /* 1). build each module's router  */
     let health_router = health::router();
     let items_router = items::router();
 
     /* 2).combine them under an aps router */
     let api_routers = Router::new()
-    .nest("/health", health_router)
-    .nest("/items", items_router);
+        .nest("/health", health_router)
+        .nest("/items", items_router);
     /* 3). mount api_router at /api/v1 */
-    
-    Router::new().nest("/api/v1", api_routers)
+    Router::new().nest(&api_base_path, api_routers)
 }
